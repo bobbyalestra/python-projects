c813a3b03707d76ef431b94bfeb2577d1e13a538
Bobby Alestra <bobbyalestra135@gmail.com>
2021-05-22 15:18:41 -0400

    update

---
+++ practice1.py
@@ -0,0 +1,18 @@
+
+
+# string concatentation 
+# suppose we wwantt o create a string thqat says "subscribe t0 _____"
+
+youtuber = "Blestra"  #some string variable
+
+# print("subscribe tio" + youtuber)
+# print("subscribe to {}".format(youtuber))
+# print(f"subscribe to{youtuber}")
+
+adjective = input("Adjective: ")
+verb1 = input("Verb: ")
+
+
+madlib = f"I love my kids they are {adjective}, funny and love able. and {verb1} "
+
+print(madlib)
